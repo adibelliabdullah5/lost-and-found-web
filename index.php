208c51d5565db8db46fd578001157c56f4c28a01
Abdullah Adibelli <adibelliabdullah5@gmail.com>
2025-12-07 18:30:02 +0300

    Add large, wide hero logo between title and Find Item button

--- index.php
+++ index.php
@@ -30,7 +30,11 @@
             <div id="site-header" style="--bg: url('<?= validate_image('assets/img/neww.jpeg?v='.time()) ?>')">
               <div class="header-content">
                 <div class="siteTitle"><?= $_settings->info('name') ?></div>
-                
+
+                <div class="hero-logo-wrap">
+                  <img class="hero-logo" src="<?= validate_image('assets/img/neww.jpeg') ?>" alt="Hero Logo" />
+                </div>
+
                 <hr class="border-light opacity-100 mx-auto" style="width:100px;border-width:3px">
                 <a href="<?= base_url.'?page=items' ?>" class="btn btn-lg btn-primary rounded-pill col-lg-3 col-md-5 col-sm-7 col-10 mx-auto d-block">Find Item</a>
               </div>
